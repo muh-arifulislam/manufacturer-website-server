81b3b87e43b7b5680b6972822be84e27c233115e
arifibnenam <arifibnenam@gmail.com>
2022-05-24 13:09:05 +0600

    manogodb added

--- index.js
+++ index.js
@@ -13,6 +13,26 @@ app.get('/', (req, res) => {
     res.send('Server running')
 })
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vzpmm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const toolCollection = client.db('manufacturerWebsite').collection('toolCollection');
+
+        // get tools 
+        app.get('/tool', async (req, res) => {
+            const tools = await toolCollection.find({}).toArray();
+            res.send(tools);
+        })
+    }
+    finally {
+        // await client.close()
+    }
+}
+run();
 app.listen(port, () => {
     console.log('Running on port', port)
 })
