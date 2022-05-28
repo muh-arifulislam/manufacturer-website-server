5eca4cb3d671c9a7130b4547f2d7d8571df4d3c5
arifibnenam <arifibnenam@gmail.com>
2022-05-28 19:53:00 +0600

    some changes

--- index.js
+++ index.js
@@ -308,6 +308,12 @@ async function run() {
             res.send(result);
         })
 
+        // get all review 
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find({}).toArray();
+            res.send(result);
+        })
+
         // add review 
         app.post('/review', async (req, res) => {
             const data = req.body;
